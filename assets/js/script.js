739c0692b46757a89e441c7804771c3e6d458a7a
Samuel <101745495+bytutscode@users.noreply.github.com>
2022-09-08 20:45:17 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+async function  getGifs  () {
+    let search = document.querySelector('#search').value;
+    document.querySelector('.gifs').innerHTML = 'Carregando...';
+    let url = `https://tenor.googleapis.com/v2/search?q=${search}&key=AIzaSyD2c5Efc-0m53ZWlDASNypLKYRzP1WBliU&client_key=my_test_app&limit=64`;
+    let json = await fetch(url);
+    json = await json.json();
+    
+    let html = '';
+    for(let i in json.results){
+        html += `<div class="col-md-3"><div class="d-flex justify-content-center p-3"><img class="img-fluid img-thumbnail" src="${json.results[i].media_formats.gif.url}" alt="${json.results[i].content_description}"></div></div>`;
+    };
+    
+    document.querySelector('.gifs').innerHTML = html;
+}
+
+let time ;
+document.querySelector('#search').addEventListener('input',()=>{
+    clearTimeout(time);
+    time = setTimeout(getGifs,500)
+});
